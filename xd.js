88b986675b191b41c3ca9b803e7937e6730f1733
alejoHayabusa <36050570+alejoHayabusa@users.noreply.github.com>
2020-10-13 19:57:04 -0500

    Add files via upload

---
+++ xd.js
@@ -0,0 +1,9 @@
+document.getElementById("si").onclick= function () {
+    alert("Ya digiste que sí no hay vuelta atras 😍😍");
+}
+
+document.getElementById("no").onmousemove=function () {
+    document.getElementById("no").style.top=(Math.random()*500)+"px";
+    document.getElementById("no").style.left=(Math.random()*500)+"px";
+}
+    
